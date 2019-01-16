c3d71a2ee4c0318872538c3cc05ed00e857f4fd2
Marc Knaup <marc@knaup.io>
2019-01-16 10:43:05 +0100

    Kotlin 1.3.20-eap-100

--- buildSrc/sources/DependencyVersions.kt
+++ buildSrc/sources/DependencyVersions.kt
@@ -4,7 +4,7 @@ object DependencyVersions {
 	const val fluid_json = "0.9.8"
 	const val fluid_mongo = "0.9.2"
 	const val jetpack = "0.1.2"
-	const val kotlin = "1.3.20-eap-52"
+	const val kotlin = "1.3.20-eap-100"
 	const val ktor = "1.1.1"
 	const val logback = "1.2.1"
 	const val versions = "0.20.0"
