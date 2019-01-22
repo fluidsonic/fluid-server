78d03d6daff880acd8f0f5f81398ee757494d88a
Marc Knaup <marc@knaup.io>
2019-01-22 19:16:25 +0100

    made RefreshToken JSON-decodable

--- sources/model/RefreshTokenJSONCodec.kt
+++ sources/model/RefreshTokenJSONCodec.kt
@@ -1,11 +1,17 @@
 package com.github.fluidsonic.baku
 
-import com.github.fluidsonic.fluid.json.AbstractJSONEncoderCodec
+import com.github.fluidsonic.fluid.json.AbstractJSONCodec
 import com.github.fluidsonic.fluid.json.JSONCodingContext
+import com.github.fluidsonic.fluid.json.JSONCodingType
+import com.github.fluidsonic.fluid.json.JSONDecoder
 import com.github.fluidsonic.fluid.json.JSONEncoder
 
 
-internal object RefreshTokenJSONCodec : AbstractJSONEncoderCodec<RefreshToken, JSONCodingContext>() {
+internal object RefreshTokenJSONCodec : AbstractJSONCodec<RefreshToken, JSONCodingContext>() {
+
+	override fun JSONDecoder<JSONCodingContext>.decode(valueType: JSONCodingType<in RefreshToken>) =
+		RefreshToken(readString())
+
 
 	override fun JSONEncoder<JSONCodingContext>.encode(value: RefreshToken) {
 		writeString(value.raw)
