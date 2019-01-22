a6b829662e5c8c11ae4c74a9c32a2031fc08cd8a
Marc Knaup <marc@knaup.io>
2019-01-22 19:24:39 +0100

    made AccessToken JSON-decodable

--- sources/model/AccessTokenJSONCodec.kt
+++ sources/model/AccessTokenJSONCodec.kt
@@ -1,11 +1,17 @@
 package com.github.fluidsonic.baku
 
-import com.github.fluidsonic.fluid.json.AbstractJSONEncoderCodec
+import com.github.fluidsonic.fluid.json.AbstractJSONCodec
 import com.github.fluidsonic.fluid.json.JSONCodingContext
+import com.github.fluidsonic.fluid.json.JSONCodingType
+import com.github.fluidsonic.fluid.json.JSONDecoder
 import com.github.fluidsonic.fluid.json.JSONEncoder
 
 
-internal object AccessTokenJSONCodec : AbstractJSONEncoderCodec<AccessToken, JSONCodingContext>() {
+internal object AccessTokenJSONCodec : AbstractJSONCodec<AccessToken, JSONCodingContext>() {
+
+	override fun JSONDecoder<JSONCodingContext>.decode(valueType: JSONCodingType<in AccessToken>) =
+		AccessToken(readString())
+
 
 	override fun JSONEncoder<JSONCodingContext>.encode(value: AccessToken) {
 		writeString(value.raw)
